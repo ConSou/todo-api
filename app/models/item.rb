59add8e84e7ef9f042274ec604309750bbd1c491
Conor Souhrada <ConorSouhrada@gmail.com>
2018-09-14 19:41:02 -0600

    add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :title, length: {minimum: 3}, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :items
+
+  validates :title, length: {minimum: 3}, presence: true
 end
