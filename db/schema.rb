6546dd3f1ed9027411a12964332607de0a35b3b9
Conor Souhrada <ConorSouhrada@gmail.com>
2018-09-14 19:35:24 -0600

    user-story-3 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_231533) do
+ActiveRecord::Schema.define(version: 2018_09_15_013127) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["list_id"], name: "index_items_on_list_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "lists", force: :cascade do |t|
